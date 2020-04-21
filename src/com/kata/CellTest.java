093c5f188ded75133c573da0c8187c837fc1e44a
Juan Jesus Padrón Hernández <jpadron@opencanarias.es>
2020-04-21 21:19:20 +0100

    refactoring Cell tests

--- src/com/kata/CellTest.java
+++ src/com/kata/CellTest.java
@@ -1,33 +1,39 @@
 package com.kata;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CellTest {
+
+    private Cell deadCell = new Cell(0);
+    private Cell livingCell = new Cell(1);
+
+    @DisplayName("Dead cell is created")
     @Test
     public void a_dead_cell_is_created() {
-        Cell cell = new Cell(0);
-        assertEquals(0, cell.getStatus(), "A dead cell is created");
+        assertEquals(0, deadCell.getStatus(), "A dead cell is created");
     }
 
+    @DisplayName("Living cell is created")
     @Test
     public void a_living_cell_is_created() {
-        Cell cell = new Cell(1);
-        assertEquals(1, cell.getStatus(), "A living cell is created");
+        assertEquals(1, livingCell.getStatus(), "A living cell is created");
     }
 
+    @DisplayName("Dead cell is represented")
     @Test
     public void a_dead_cell_is_correctly_represented() {
-        Cell cell = new Cell(0);
-        assertEquals("·", cell.toString(), "The dead cell is represented correctly by the character ·");
+        assertEquals("·", deadCell.toString(), "The dead cell is represented correctly by the character ·");
     }
 
+    @DisplayName("Living cell is created")
     @Test
     public void a_living_cell_is_correctly_represented() {
-        Cell cell = new Cell(1);
-        assertEquals("x", cell.toString(), "The dead cell is represented correctly by the character x");
+        assertEquals("x", livingCell.toString(), "The dead cell is represented correctly by the character x");
     }
 
 }
