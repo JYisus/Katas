46d02f840fdf0e6f3f02d09e074f8d7cec5d259a
Juan Jesús Padrón Hernández <jpadron@opencanarias.es>
2020-04-30 21:57:46 +0100

    Kata success, needs refactor

--- src/WordWrapper.java
+++ src/WordWrapper.java
@@ -1,7 +1,12 @@
 public class WordWrapper {
     public static String WrapText(String text, int columnWidth) {
-        String wrappedText = text.substring(0,columnWidth).concat("\n");
-        String remainingText = text.substring(columnWidth);
+        String wrappedText = ""; //text.substring(0,columnWidth);
+        String remainingText = text;
+        while(remainingText.length() > columnWidth) {
+            wrappedText = wrappedText.concat(remainingText.substring(0, columnWidth)).concat("\n");
+            remainingText = remainingText.substring(columnWidth);
+        }
+
         return wrappedText.concat(remainingText);
     }
 }
--- src/WordWrapperTest.java
+++ src/WordWrapperTest.java
@@ -14,4 +14,16 @@ class WordWrapperTest {
         String text = "hola";
         assertEquals("ho\nla", WordWrapper.WrapText(text, 2));
     }
+
+    @Test
+    public void aTextWithLengthMultipleOfColumnWidth() {
+        String text = "hola qué tal";
+        assertEquals("ho\nla\n q\nué\n t\nal", WordWrapper.WrapText(text, 2));
+    }
+
+    @Test
+    public void aTextWithLengthNOMultipleOfColumnWidth() {
+        String text = "hola qué tal?";
+        assertEquals("ho\nla\n q\nué\n t\nal\n?", WordWrapper.WrapText(text, 2));
+    }
 }
