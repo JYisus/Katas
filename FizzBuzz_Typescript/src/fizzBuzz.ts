6063c64e829d983993d562009bcf652a38fad879
Juan Jesús Padrón Hernández <jpadron@opencanarias.es>
2020-09-07 16:33:22 +0100

    Class refactor

--- FizzBuzz_Typescript/src/fizzBuzz.ts
+++ FizzBuzz_Typescript/src/fizzBuzz.ts
@@ -1,14 +1,28 @@
-export function fizzBuzz(givenNumber: number): number | string {
-    if(isDivisibleBy3(givenNumber) && isDivisibleBy5(givenNumber)) return "FizzBuzz";
-    if(isDivisibleBy3(givenNumber)) return "Fizz";
-    if(isDivisibleBy5(givenNumber)) return "Buzz";
-    return givenNumber;
+export class FizzBuzz {
+    generate(lastNumber: number): string {
+        const results: string[] = []
+        for (let i = 1; i <= lastNumber; i++) {
+            results.push(this.translateNumber(i))
+        }
+        return results.join(" ")
+    }
+
+    translateNumber(givenNumber: number): string {
+        if(this.isDivisibleBy3and5(givenNumber)) return "FizzBuzz";
+        if(this.isDivisibleBy3(givenNumber)) return "Fizz";
+        if(this.isDivisibleBy5(givenNumber)) return "Buzz";
+        return givenNumber.toString();
+    }
+    private isDivisibleBy3(n: number): boolean {
+        return n%3 == 0;
+    }
+    
+    private isDivisibleBy5(n: number): boolean {
+        return n%5 == 0;
+    }
+
+    private isDivisibleBy3and5(n: number): boolean {
+        return n%15 == 0;
+    }
 }
 
-function isDivisibleBy3(n: number): boolean {
-    return n%3 == 0;
-}
-
-function isDivisibleBy5(n: number): boolean {
-    return n%5 == 0;
-}
--- FizzBuzz_Typescript/tests/fizzBuzz.test.ts
+++ FizzBuzz_Typescript/tests/fizzBuzz.test.ts
@@ -1,18 +1,23 @@
-import { fizzBuzz } from '../src/fizzBuzz';
+import { FizzBuzz } from '../src/FizzBuzz';
 //const fb = require('../src/fizzBuzz');
+const fb = new FizzBuzz();
 
-test('when give a number not divisible by 3 or 5, return the same number', () => {
-  expect(fizzBuzz(1)).toBe(1);
+test('when give a number not divisible by 3 or 5, return the same number', () => { 
+  expect(fb.translateNumber(1)).toBe("1");
 })
 
 test('when give a number divisible by 3, return Fizz', () => {
-  expect(fizzBuzz(3)).toBe("Fizz");
+  expect(fb.translateNumber(3)).toBe("Fizz");
 })
 
 test('when give a number divisible by 5, return Buzz', () => {
-  expect(fizzBuzz(5)).toBe("Buzz");
+  expect(fb.translateNumber(5)).toBe("Buzz");
 })
 
 test('when give a number divisible by 3 and 5, return FizzBuzz', () => {
-  expect(fizzBuzz(15)).toBe("FizzBuzz");
+  expect(fb.translateNumber(15)).toBe("FizzBuzz");
+})
+
+test('given a number generates a list of numbera from 1 to it with the FizzBuzz rules', () => {
+  expect(fb.generate(20)).toBe("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz");
 })
