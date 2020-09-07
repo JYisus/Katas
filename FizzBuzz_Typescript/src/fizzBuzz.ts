4fbc44930d3268d26111fda9e22a49a38bc925ce
Juan Jesús Padrón Hernández <jpadron@opencanarias.es>
2020-09-07 16:53:11 +0100

    SOLID refactor

--- FizzBuzz_Typescript/src/fizzBuzz.ts
+++ FizzBuzz_Typescript/src/fizzBuzz.ts
@@ -1,4 +1,8 @@
 export class FizzBuzz {
+    constructor(private rules: TranslateRule[]) {}
+    addRule(newRule: TranslateRule) {
+        this.rules.push(newRule);
+    }
     generate(lastNumber: number): string {
         const results: string[] = []
         for (let i = 1; i <= lastNumber; i++) {
@@ -8,21 +12,43 @@ export class FizzBuzz {
     }
 
     translateNumber(givenNumber: number): string {
-        if(this.isDivisibleBy3and5(givenNumber)) return "FizzBuzz";
-        if(this.isDivisibleBy3(givenNumber)) return "Fizz";
-        if(this.isDivisibleBy5(givenNumber)) return "Buzz";
+        for (const rule of this.rules) {
+            if (rule.match(givenNumber)) {
+                return rule.translateNumber(givenNumber);
+            }
+        }
         return givenNumber.toString();
-    }
-    private isDivisibleBy3(n: number): boolean {
-        return n%3 == 0;
-    }
-    
-    private isDivisibleBy5(n: number): boolean {
-        return n%5 == 0;
-    }
-
-    private isDivisibleBy3and5(n: number): boolean {
-        return n%15 == 0;
     }
 }
 
+interface TranslateRule {
+    match(givenNumber: number): boolean;
+    translateNumber(givenNumber: number): string;
+}
+
+export class FizzRule implements TranslateRule {
+    match(givenNumber: number): boolean {
+        return givenNumber%3 == 0;
+    }
+    translateNumber(): string {
+        return "Fizz";
+    }
+}
+
+export class BuzzRule implements TranslateRule {
+    match(givenNumber: number): boolean {
+        return givenNumber%5 == 0;
+    }
+    translateNumber(): string {
+        return "Buzz";
+    }
+}
+
+export class FizzBuzzRule implements TranslateRule {
+    match(givenNumber: number): boolean {
+        return givenNumber%15 == 0;
+    }
+    translateNumber(): string {
+        return "FizzBuzz"
+    }
+}
