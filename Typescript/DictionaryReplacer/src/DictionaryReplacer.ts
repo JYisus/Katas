898f9e532c1d21d5c8871910c637d5daefe6f462
Juan Jesús Padrón Hernández <jpadron@opencanarias.es>
2020-09-16 00:52:55 +0100

    [DictionaryReplacer] Ultra refactor! (Estaba haciendo demasiada sobreingeniería, esta solución es bastante más sencilla y legible)

--- Typescript/DictionaryReplacer/src/DictionaryReplacer.ts
+++ Typescript/DictionaryReplacer/src/DictionaryReplacer.ts
@@ -2,28 +2,10 @@ class DictionaryReplacer {
     replace(stringBeforeReplace: string, dictionary: Object): string {
         if (stringBeforeReplace === "" || Object.keys(dictionary).length === 0) return "";
 
-        const stringSplited: string[] = stringBeforeReplace.split(" ");
-
-        let finalStringSparsed: string[] = stringSplited.map((word) => this.replaceWordsBetweenDollars(word,dictionary))
-
-        return finalStringSparsed.join(" ");
-    }
-
-    private replaceWordsBetweenDollars(word: string, dictionary: Object): string {
-
-        if (word.match('.*\$.*\$.*')) {
-            const firstDollar: number = word.indexOf("$");
-            const secondDollar: number = word.indexOf("$",firstDollar+1);
-            const stringBeforDollar: string = word.slice(0,firstDollar);
-            const stringAfterDollar: string = word.slice(secondDollar+1, word.length)
-            const wordWithoutDollars: string = word.slice(firstDollar+1,secondDollar);
-            const isWordInDictionary: boolean = dictionary.hasOwnProperty(wordWithoutDollars);
-            const wordSubstituted: string = dictionary[wordWithoutDollars];
-            
-             if (isWordInDictionary) return stringBeforDollar+wordSubstituted+stringAfterDollar;
+        for(let word of Object.keys(dictionary)) {
+            stringBeforeReplace = stringBeforeReplace.replace(`\$${word}\$`,dictionary[word]);
         }
-
-        return word;
+        return stringBeforeReplace;
     }
 }
 
