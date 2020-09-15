d26147403e9bcd5673d5d335f6a557acf1016030
Juan Jesús Padrón Hernández <jpadron@opencanarias.es>
2020-09-15 21:37:55 +0100

    [DictionaryReplacer] Functional refactor

--- Typescript/DictionaryReplacer/src/DictionaryReplacer.ts
+++ Typescript/DictionaryReplacer/src/DictionaryReplacer.ts
@@ -1,21 +1,25 @@
 class DictionaryReplacer {
-    replace(stringBeforeReplace: string, dictionary: Object) {
+    replace(stringBeforeReplace: string, dictionary: Object): string {
         if (stringBeforeReplace === "" || Object.keys(dictionary).length === 0) return "";
 
         let finalStringSparsed: string[] = [];
-        for (let word of stringBeforeReplace.split(" ")) {
-            const wordWithoutDollars: string = word.slice(1,word.length-1);
-            const isWordInDictionary: boolean = dictionary.hasOwnProperty(wordWithoutDollars);
 
-            if (word.match('\$.*\$') && isWordInDictionary) {
-                const wordSubstituted: string = dictionary[wordWithoutDollars];
-                finalStringSparsed.push(wordSubstituted);
-            } else {
-                finalStringSparsed.push(word);
-            }
-        }
+        finalStringSparsed = stringBeforeReplace.split(" ").map((word) => this.replaceWordsBetweenDollars(word,dictionary))
+        
 
         return finalStringSparsed.join(" ");
+    }
+
+    private replaceWordsBetweenDollars(word: string, dictionary: Object): string {
+        const wordWithoutDollars: string = word.slice(1,word.length-1);
+        const isWordInDictionary: boolean = dictionary.hasOwnProperty(wordWithoutDollars);
+
+        if (word.match('\$.*\$') && isWordInDictionary) {
+            const wordSubstituted: string = dictionary[wordWithoutDollars];
+            return wordSubstituted;
+        }
+
+        return word;
     }
 }
 
